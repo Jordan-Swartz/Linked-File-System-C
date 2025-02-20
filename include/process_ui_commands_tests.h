00fbf7ff21d927e57074abab23ba8f5a5efeb4e1
Jordan <JDSwartz@proton.me>
2025-02-19 23:55:44 -0800

    Added testing files and modified CMakeLists.txt for testing support

---
+++ include/process_ui_commands_tests.h
@@ -0,0 +1,10 @@
+#ifndef PROCESS_UI_COMMANDS_TESTS_C_H
+#define PROCESS_UI_COMMANDS_TESTS_C_H
+
+#include "unity.h"
+#include "ui.h"
+
+void test_validate_args_single_arg_success(void);
+void test_validate_args_single_arg_fail(void);
+
+#endif //PROCESS_UI_COMMANDS_TESTS_C_H
