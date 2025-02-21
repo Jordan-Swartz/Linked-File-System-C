4ae64d5b7d68554691999d305c34fb9288354604
Jordan <JDSwartz@proton.me>
2025-02-20 21:09:59 -0800

    Updated comments and documentation for memory_management.c and memory_management.h

--- include/memory_management.h
+++ include/memory_management.h
@@ -1,7 +1,21 @@
+/**
+ * @file memory_management.h
+ * @brief Defines the methods for memory management in the file system.
+ *
+ * @author Jordan Swartz
+ * @version 1.0
+ * @date 2025-02-20
+ */
+
 #ifndef MEMORY_MANAGEMENT_H
 #define MEMORY_MANAGEMENT_H
 
-//functions
+/*Function Declarations*/
+/**
+ * Frees memory for an array of strings.
+ *
+ * @param path The array of strings.
+ */
 void free_path(char** path);
 
 #endif //MEMORY_MANAGEMENT_H
--- src/util/memory_management.c
+++ src/util/memory_management.c
@@ -3,18 +3,17 @@
  *
  * allocate memory block when a file grows (sim disk space on file)
  * deallocate memory when file is deleted
- * handle framentation
+ * handle fragmentation
  * track total memory management (usage, disk space)
  * delete file
  *
  *
- * create and deleete memory for nodes
+ * create and delete memory for nodes
  */
 
 #include <stdlib.h>
 #include "memory_management.h"
 
-//free array of strings
 void free_path(char** path) {
     int i = 0;
     while (path[i] != NULL) {
