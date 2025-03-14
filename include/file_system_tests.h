f16866d2af7fe898bc5a1f8615179f604eeb57e6
Jordan <JDSwartz@proton.me>
2025-03-13 23:39:37 -0700

    Added unit tests in file_system_tests.c for core logic in file_system.c.

--- include/file_system_tests.h
+++ include/file_system_tests.h
@@ -1,8 +1,73 @@
-//
-// Created by jordan on 3/13/25.
-//
+/**
+* @file file_system_tests.h
+ * @brief Defines the test methods for the file system core logic.
+ *
+ * This header file declares unit test functions for validating the core logic of the file system.
+ * The tests ensure that the core logic works as intended.
+ *
+ * @author Jordan Swartz
+ * @version 1.0
+ * @date 2025
+ */
 
 #ifndef FILE_SYSTEM_TESTS_H
 #define FILE_SYSTEM_TESTS_H
+
+#include "unity.h"
+#include "file_system.h"
+#include <stdlib.h>
+#include <string.h>
+#include <ui.h>
+
+/**
+ * Tests system_setup function by initializing system and verifying attributes.
+ */
+void test_system_setup(void);
+
+/**
+ * Tests root_setup function setting and verifying the root node attributes.
+ */
+void test_root_setup(void);
+
+/**
+ * Tests create_node function by creating a FSNode.
+ */
+void test_create_node(void);
+
+/**
+ * Tests create_copy_node function by duplicating FSNode attributes.
+ */
+void test_create_copy_node(void);
+
+/**
+ * Tests insert_node function to ensure nodes are inserted alphabetically.
+ * Verifies that duplicates are not added.
+ */
+void test_insert_node(void);
+
+/**
+ * Tests recursive_copy function to validate deep copying of directories.
+ */
+void test_recursive_copy(void);
+
+/**
+ * Tests recursive_delete function to verify directory contents are freed without crashing.
+ */
+void test_recursive_delete(void);
+
+/**
+ * Tests change_directory_forward function for navigating directories.
+ */
+void test_change_directory_forward(void);
+
+/**
+ * Tests is_subdirectory function to ensure correct subdirectory detection.
+ */
+void test_is_subdirectory(void);
+
+/**
+ * Tests find_node function to ensure nodes are correctly found.
+ */
+void test_find_node(void);
 
 #endif //FILE_SYSTEM_TESTS_H
--- test/main_tests.c
+++ test/main_tests.c
@@ -1,1 +1,27 @@
 #include "main_tests.h"
+
+void setUp(void) {}
+void tearDown(void) {}
+
+int main(void) {
+    UNITY_BEGIN();
+
+    //file_system_tests
+    RUN_TEST(test_system_setup);
+    RUN_TEST(test_root_setup);
+    RUN_TEST(test_create_node);
+    RUN_TEST(test_create_copy_node);
+    RUN_TEST(test_insert_node);
+    RUN_TEST(test_recursive_copy);
+    RUN_TEST(test_recursive_delete);
+    RUN_TEST(test_change_directory_forward);
+    RUN_TEST(test_is_subdirectory);
+    RUN_TEST(test_find_node);
+
+
+    //ui_tests
+    RUN_TEST(test_validate_args_single_arg_success);
+    RUN_TEST(test_validate_args_single_arg_fail);
+
+    return UNITY_END();
+}
